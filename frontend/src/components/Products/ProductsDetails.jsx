64163c48fce49e3eb22317562401577105a50201
AnouarDev <anouar_leeroy@hotmail.com>
2025-10-31 10:27:22 +0100

    update: added scrollable product section + best sellers + new components

---
+++ frontend/src/components/Products/ProductsDetails.jsx
@@ -0,0 +1,88 @@
+const selectedProduct ={
+    name: "Product 1",
+    price: 120,
+    originalPrice: 150,
+    description:"This is a Product 1",
+     material:"Original",
+     weight:["5 KG","10 KG","15 KG","20 KG"],
+     colors:["Red","Black"],
+     images:[
+        {
+        url:"https://picsum.photos/500/500?random=1",
+        alterText:"Product 1",
+     },
+    {
+        url:"https://picsum.photos/500/500?random=2",
+        alterText:"Product 2",
+     },
+    ],
+};
+
+const ProductsDetails = () => {
+  return (
+    <div className="p-6">
+        <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg">
+            <div className="flex flex-col md:flex-row">
+                {/* Left Thumbnails */}
+                <div className="hidden md:flex flex-col space-y-4 mr-6">
+                 {selectedProduct.images.map((image, index) => (
+                <img
+                key={index}
+                 src={image.url} 
+                 alt={image.altText || `Thumbnail ${index}`}
+                className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+                 />
+))}  
+                </div>
+                {/* Main Image */}
+                <div className="md:w-1/2">
+                <div className="mb-4">
+                    <img src={selectedProduct.images[0]?.url} alt="Main Product"
+                    className="w-full h-auto object-cover rounded-lg" 
+                    />
+                </div>
+                </div>
+                {/* Mobile Thumbnail */}
+                <div className="md:hidden flex overscroll-x-scroll space-x-4 mb-4">
+                 {selectedProduct.images.map((image, index) => (
+                <img
+                key={index}
+                 src={image.url} 
+                 alt={image.altText || `Thumbnail ${index}`}
+                className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+                 />
+))}   
+                </div>
+                {/* Right Side */}
+                <div className="md:w-1/2 md:ml-10">
+                <h1 className="text-2xl md:text-3xl font-semibold mb-2">
+                    {selectedProduct.name}
+                </h1>
+                <p className="text-lg text-gray-600 mb-1 line-through">
+                    {selectedProduct.originalPrice && `${selectedProduct.originalPrice}`}
+                </p>
+                <p className="text-xl text-gray500 mb-2">
+                    $ {selectedProduct.price}
+                </p>
+                <p className="text-gray-600 mb-4">
+                    {selectedProduct.description}
+                </p>
+                <div className="mb-4">
+                    <p className="text-gray-700">Color:</p>
+                    <div className="flex gap-2 mt-2">
+                        {selectedProduct.colors.map((color) => (
+                            <button
+                             key={color} 
+                             className="w-8 h-8 rounded-full border"
+                             ></button>
+                        ))}
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductsDetails
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import Hero from "../components/Layout/Hero";
 import GymCollectionSection from "../components/Products/GymCollectionSection";
+import NewArrivals from "../components/Products/NewArrivals";
+import ProductsDetails from "../components/Products/ProductsDetails";
 const Home = () => {
   return (
     <div>
         <Hero />
         <GymCollectionSection />
+        <NewArrivals />
+
+        {/* Best Sellers Section*/}
+        <h2 className="text-3xl text-center font-bold mb-4"> Best Seller</h2>
+        <ProductsDetails />
     </div>
   )
 }
